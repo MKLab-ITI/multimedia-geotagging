bcec5ee27d013eecc5519e7267a710214ee972dc
Giorgos Kordopatis-Zilos <georgekordopatis@hotmail.com>
2018-10-17 00:47:41 +0300

    fix broken link

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
-wget https://www.dropbox.com/s/7vdyarczp97r60c/multi-geo-utils.zip
+wget https://www.dropbox.com/s/axe3vkj29p9z4o6/multi-geo-utils.zip
 unzip multi-geo-utils.zip -d multi-geo-utils
 rm multi-geo-utils.zip
